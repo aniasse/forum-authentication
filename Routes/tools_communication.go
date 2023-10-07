f06230d418629c92bfd6747096949360e9840852
mthiaw <massaw6@gmail.com>
2023-10-07 20:50:50 +0000

    changes: optimized code with error handling

--- Routes/tools_communication.go
+++ Routes/tools_communication.go
@@ -2,9 +2,9 @@ package Route
 
 import (
 	"fmt"
+	Err "forum/Authentification"
 	db "forum/Database"
 	tools "forum/tools"
-	"html/template"
 	"net/http"
 	"strings"
 )
@@ -15,9 +15,7 @@ func CreateP_mngmnt(w http.ResponseWriter, r *http.Request, categorie []string, 
 	errpost := postab.Create_post(database, Id_user, categorie, title, content, image)
 	if errpost != nil {
 		fmt.Printf("⚠ ERROR ⚠ : Couldn't create post from user %s ❌\n", Id_user)
-		w.WriteHeader(http.StatusInternalServerError)
-		error_file := template.Must(template.ParseFiles("templates/error.html"))
-		error_file.Execute(w, "500")
+		Err.Snippets(w, 500)
 		return
 	}
 	fmt.Println("post created with content ", content)
@@ -26,10 +24,12 @@ func CreateP_mngmnt(w http.ResponseWriter, r *http.Request, categorie []string, 
 	//---formatting content to escape special chars
 	content = strings.ReplaceAll(content, "'", "2@c86cb3")
 	content = strings.ReplaceAll(content, "`", "2#c86cb3")
+
 	//---fetching id post in database
 	condition := fmt.Sprintf("WHERE %s = '%s'", db.Description, content)
 	Idpost, err1 := database.GetData(db.Id_post, db.Post, condition)
 	Idpost_got, err2 := db.Getelement(Idpost)
+
 	if err1 != nil && err2 != nil {
 		http.Redirect(w, r, redirect+"#"+Idpost_got, http.StatusSeeOther)
 	} else { //no id found in database, post creation encountered a problem
@@ -42,9 +42,7 @@ func CreateC_mngmnt(w http.ResponseWriter, r *http.Request, Id_post string, newc
 	errcomm := commtab.Create_comment(database, Id_user, Id_post, newcomment)
 	if errcomm != nil {
 		fmt.Printf("⚠ ERROR ⚠ : Couldn't create comment in post %s from user %s ❌\n", Id_post, Id_user)
-		w.WriteHeader(http.StatusInternalServerError)
-		error_file := template.Must(template.ParseFiles("templates/error.html"))
-		error_file.Execute(w, "500")
+		Err.Snippets(w, 500)
 		return
 	}
 }
@@ -59,9 +57,7 @@ func ReplyC_mngmnt(w http.ResponseWriter, r *http.Request, Id_post string, Id_co
 	errcomm := commtab.Create_comment(database, Id_user, Id_post, reply)
 	if errcomm != nil {
 		fmt.Printf("⚠ ERROR ⚠ : Couldn't create reply to comment %s , in on post %s from user %s ❌\n", Id_comment, Id_post, Id_user)
-		w.WriteHeader(http.StatusInternalServerError)
-		error_file := template.Must(template.ParseFiles("templates/error.html"))
-		error_file.Execute(w, "500")
+		Err.Snippets(w, 500)
 		return
 	}
 
