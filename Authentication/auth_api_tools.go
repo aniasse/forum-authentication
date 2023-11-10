6945092b0960c15999090531d2ee3a11b7609e56
mthiaw <massaw6@gmail.com>
2023-11-10 15:21:06 +0000

    github client secret updated

--- Authentication/auth_api_tools.go
+++ Authentication/auth_api_tools.go
@@ -8,10 +8,10 @@ const (
 	GoAuthURL      = "https://accounts.google.com/o/oauth2/auth"                                // url to ask for access permission
 	GoTokenURL     = "https://accounts.google.com/o/oauth2/token"                               // url to exchange permission with access token
 	GoUserInfoURL  = "https://www.googleapis.com/oauth2/v2/userinfo"                            // url to exchange token with user info
-	
+
 	// -- github side
 	GitClientID     = "d0e6a8ea96ab8fe09e42"                     // our application id
-	GitClientSecret = "34f879339f86bf9dfb85f47506a2ccf334c34a15" // our secret client id
+	GitClientSecret = "a00571f86a06be74e121c692a55383a209f3911c" // our secret client id
 	GitRedirectURI  = "http://localhost:8080/auth/github/callback"
 	GitAuthURL      = "https://github.com/login/oauth/authorize"
 )
